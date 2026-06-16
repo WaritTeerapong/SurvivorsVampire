9fd9dfffa0b8d67d5753d9a8d765cce05b6c361d
WaritTeerapong <smart29952@gmail.com>
2026-06-16 11:16:24 +0700

    Add item SOs, base class and databases
    Introduce a data-driven item system: adds a generic ItemData_Base<T> ScriptableObject with Id/ItemName/Icon/BonusPerLevel and GetBonusForLevel handling bounds. Adds WeaponItemData_SO (with WeaponStat and prefab) and PassiveItemData_SO types. Implements WeaponItemDatabase_SO and PassiveItemDatabase_SO that build O(1) lookup dictionaries (Initialize + GetItemByID). Also add example data assets (Armour, Shoes, Gun) and corresponding database asset files and .meta entries.

---
+++ Assets/Scripts/SOScripts/Item/WeaponItemData_SO.cs
@@ -0,0 +1,16 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+[System.Serializable]
+public struct WeaponStat
+{
+    public float AtkDamage; 
+    public float AtkRange;
+    public float AtkCooldown;
+}
+
+[CreateAssetMenu(fileName = "WeaponItemData_So", menuName = "DataSO/Item/WeaponItemData_SO")]
+public class WeaponItemData_SO : ItemData_Base<WeaponStat>
+{
+    public GameObject WeaponPrefab;
+}
