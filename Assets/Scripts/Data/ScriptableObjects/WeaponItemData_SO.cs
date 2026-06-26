bc53a81efe61416ad9b83c2bf9728390ab6ee9b7
Bobbie <kobk708@gmail.com>
2026-06-26 17:47:22 +0700

    Merge pull request #53 from WaritTeerapong/feat/Game-Juice
    Feat/game juice

---
+++ Assets/Scripts/Data/ScriptableObjects/WeaponItemData_SO.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct WeaponStat
+{
+    public float ATKDamage;
+    public float ATKRange;
+    public float ATKSpeed;
+}
+
+[CreateAssetMenu(fileName = "WeaponItemData_So", menuName = "DataSO/Item/WeaponItemData_SO")]
+public class WeaponItemData_SO : ItemData_Base<WeaponStat>
+{
+    public GameObject WeaponPrefab;
+
+    [Header("=== VFX Settings ===")]
+    public GameObject HitVFXPrefab;
+}
