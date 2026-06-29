5f5a4d1f289efe737226b29d0c8103e60b86daee
WaritTeerapong <smart29952@gmail.com>
2026-06-29 15:03:24 +0700

    Revert "Merge pull request #53 from WaritTeerapong/feat/Game-Juice"
    This reverts commit 1f8121246ca2c99269622fc2c260f8a17e12ef89, reversing
    changes made to a58cc72fe78909d9b4ad74acd04abc0510ae0a67.

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,372 @@
+using System;
+using System.Collections;
+using DG.Tweening;
+using Unity.Netcode;
+using UnityEngine;
+
+public struct EnemyCurrentStats : INetworkSerializable
+{
+    public int EnemyID;
+    public int Tier;
+    public int CurrentHealth;
+    public int MoveSpeed;
+    public int ATKDamage;
+    public float ATKSpeed;
+    public float ATKRange;
+    public float ColorR;
+    public float ColorG;
+    public float ColorB;
+
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+        serializer.SerializeValue(ref EnemyID);
+        serializer.SerializeValue(ref Tier);
+        serializer.SerializeValue(ref CurrentHealth);
+        serializer.SerializeValue(ref MoveSpeed);
+        serializer.SerializeValue(ref ATKDamage);
+        serializer.SerializeValue(ref ATKSpeed);
+        serializer.SerializeValue(ref ATKRange);
+        serializer.SerializeValue(ref ColorR);
+        serializer.SerializeValue(ref ColorG);
+        serializer.SerializeValue(ref ColorB);
+    }
+
+}
+
+public class Enemy : NetworkBehaviour
+{
+    [Header("Component refernce")]
+    public EnemyDetector Detector;
+    public EnemyMovement Movement;
+    public EnemyCombat Combat;
+
+    [Header("Bullet Prefab")]
+    public GameObject BulletPrefab;
+
+    [Header("Eneym Type SO")]
+    public EnemyTypeData_SO EnemyType;
+
+    public Vector2 CurrentDirection { get; private set; }
+
+    private Animator _anim;
+    private Vector3 _lastPosition;
+    private bool _isDead = false;
+
+    private Collider2D col;
+
+    [Header("=== Hit Flash Material ===")]
+    public Material HitFlashMaterial;
+    private Material _originalMaterial;
+    private SpriteRenderer _spriteRenderer;
+    private Tween _flashTween;
+
+    public NetworkVariable<EnemyCurrentStats> CurrentStats = new NetworkVariable<EnemyCurrentStats>(
+        new EnemyCurrentStats(),
+        readPerm: NetworkVariableReadPermission.Everyone,
+        writePerm: NetworkVariableWritePermission.Server
+    );
+
+    public NetworkVariable<float> FacingDirection = new NetworkVariable<float>(
+        1f,
+        readPerm: NetworkVariableReadPermission.Everyone,
+        writePerm: NetworkVariableWritePermission.Server
+    );
+
+    // C# event
+    public event Action<EnemyCurrentStats> OnEnemyStatsChanged;
+    public event Action<Enemy> OnEnemyDespawned;
+
+    // === FSM ( Finite State-Machine ) ===
+    #region     FSM State-Machine
+    public readonly IEnemyState IdleState = new EnemyIdleState();
+    public readonly IEnemyState MoveState = new EnemyMoveState();
+    public readonly IEnemyState AttackState = new EnemyAttackState();
+    public readonly IEnemyState DieState = new EnemyDieState();
+    private IEnemyState _currentState;
+    #endregion
+
+    public static readonly int IDLE = Animator.StringToHash("Idle");
+    public static readonly int RUN = Animator.StringToHash("Run");
+    public static readonly int ATK = Animator.StringToHash("Attack");
+    public static readonly int DIE = Animator.StringToHash("Die");
+
+    private void Awake()
+    {
+        _anim = GetComponentInChildren<Animator>();
+        col = GetComponent<Collider2D>();
+
+        _spriteRenderer = _anim != null ? _anim.GetComponent<SpriteRenderer>() : GetComponentInChildren<SpriteRenderer>();
+        if (_spriteRenderer != null)
+        {
+            _originalMaterial = _spriteRenderer.material;
+        }
+    }
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        CurrentStats.OnValueChanged += OnEnemyStatsValueChanged;
+
+        ApplyTierColor(CurrentStats.Value);
+
+        if (IsServer && EnemySpawnManager.Instance != null)
+        {
+            _isDead = false;
+            SetColliderTo(true);
+
+            Detector?.StartDetect();
+            SwitchState(IdleState);
+        }
+        else if (IsServer) // Check if Manager not Instance
+        {
+            Debug.LogError("EnemySpawnManager is missing on " + gameObject.name);
+        }
+
+        // DebugLogStatsRpc();
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        base.OnNetworkDespawn();
+        CurrentStats.OnValueChanged -= OnEnemyStatsValueChanged;
+
+        if (IsServer)
+        {
+            Detector?.StopDetect();
+
+            OnEnemyDespawned?.Invoke(this);
+            OnEnemyDespawned = null;
+
+            EnemyType = null;
+            _currentState = null;
+        }
+    }
+
+    public void SetColliderTo(bool isEnable) => col.enabled = isEnable;
+
+    private void OnEnemyStatsValueChanged(EnemyCurrentStats previousValue, EnemyCurrentStats newValue)
+    {
+        if (newValue.CurrentHealth < previousValue.CurrentHealth)
+        {
+            if (_spriteRenderer != null && HitFlashMaterial != null)
+            {
+                _flashTween?.Kill();
+
+                _spriteRenderer.material = HitFlashMaterial;
+
+                _flashTween = DOVirtual.DelayedCall(0.15f, () =>
+                {
+                    if (_spriteRenderer != null)
+                    {
+                        _spriteRenderer.material = _originalMaterial;
+                    }
+                });
+            }
+        }
+        else if (previousValue.Tier != newValue.Tier || previousValue.EnemyID != newValue.EnemyID)
+        {
+            ApplyTierColor(newValue);
+        }
+
+        OnEnemyStatsChanged?.Invoke(newValue);
+    }
+
+    public void InitStats(EnemyTypeData_SO enemyType, int tierLevel)
+    {
+        EnemyType = enemyType;
+
+        EnemyTier currentTierData = EnemyType.Setup(tierLevel);
+        EnemyStats _stats = currentTierData.enemyStats;
+        Color _color = currentTierData.color;
+
+        EnemyCurrentStats initStats = new EnemyCurrentStats
+        {
+            EnemyID = currentTierData.EnemyID,
+            Tier = tierLevel,
+            CurrentHealth = _stats.MaxHealth,
+            MoveSpeed = _stats.MoveSpeed,
+            ATKDamage = _stats.ATKDamage,
+            ATKSpeed = _stats.ATKSpeed,
+            ATKRange = _stats.ATKRange,
+            ColorR = _color.r,
+            ColorG = _color.g,
+            ColorB = _color.b
+        };
+        CurrentStats.Value = initStats;
+
+
+    }
+
+    public void SwitchState(IEnemyState newState)
+    {
+        if (!IsServer) return;
+
+        _currentState?.OnExit(this);
+        _currentState = newState;
+        _currentState?.OnEnter(this);
+    }
+
+    public bool IsPlayerInATKRange()
+    {
+        if (Detector == null || Detector.NearestTarget == null) return false;
+
+        float atkRange = CurrentStats.Value.ATKRange;
+
+        float currentSqrDistance = (Detector.NearestTarget.position - transform.position).sqrMagnitude;
+
+        return currentSqrDistance <= (atkRange * atkRange);
+    }
+
+    private void Update()
+    {
+        if (_isDead) return;
+        FacingToDirection();
+        _currentState?.OnUpdate(this);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (!IsServer) return;
+
+        if (DamagePopupManager.Instance != null)
+        {
+            DamagePopupManager.Instance.ShowPopup(transform.position, damage, false);
+        }
+
+        EnemyCurrentStats stats = CurrentStats.Value;
+        stats.CurrentHealth -= damage;
+        if (stats.CurrentHealth <= 0)
+        {
+            _isDead = true;
+            stats.CurrentHealth = 0;
+            Despawn();
+        }
+
+        CurrentStats.Value = stats;
+    }
+
+    public void PlayAnimation(int animation)
+    {
+        if (!IsServer) return;
+        _anim.Play(animation);
+    }
+
+    private void Despawn()
+    {
+        if (!IsServer) return;
+        SwitchState(DieState);
+        // Spawn XP Orb
+        if (XPDropManager.Instance != null && EnemyType != null)
+        {
+            XPDropManager.Instance.DropXP(transform.position, EnemyType.XPValue);
+        }
+
+        // Despawn enemy obj after 1.2 s
+        StartCoroutine(DelayDespawnRoutine(1.2f));
+    }
+    private IEnumerator DelayDespawnRoutine(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if (NetworkObject != null && NetworkObject.IsSpawned)
+        {
+            NetworkObject.Despawn(true);
+        }
+    }
+    private void ApplyTierColor(EnemyCurrentStats _stat)
+    {
+        if (_anim == null) return;
+
+        SpriteRenderer renderer = _anim.GetComponent<SpriteRenderer>() ?? GetComponentInChildren<SpriteRenderer>();
+        if (renderer == null) return;
+
+        if (_stat.ColorR == 0 && _stat.ColorG == 0 && _stat.ColorB == 0)
+        {
+            renderer.color = Color.white;
+            return;
+        }
+        renderer.color = new Color(_stat.ColorR, _stat.ColorG, _stat.ColorB, 1f);
+
+    }
+
+    private void FacingToDirection()
+    {
+        transform.localScale = new Vector3(FacingDirection.Value, 1, 1);
+
+        if (!IsServer) return;
+        Vector3 positionDelta = Vector3.zero;
+
+        // If found target, face to target 
+        if (Detector != null && Detector.NearestTarget != null)
+        {
+            positionDelta = Detector.NearestTarget.position - transform.position;
+        }
+
+        // If not found target, face to where you move
+        if (Detector.NearestTarget == null)
+        {
+            positionDelta = transform.position - _lastPosition;
+        }
+
+        if (positionDelta.x > 0.001f) FacingDirection.Value = 1f;
+        else if (positionDelta.x < -0.001f) FacingDirection.Value = -1f;
+
+        Vector2 moveDir = transform.position - _lastPosition;
+        if (moveDir != Vector2.zero)
+        {
+            CurrentDirection = moveDir.normalized;
+        }
+
+        _lastPosition = transform.position;
+    }
+
+    [Rpc(SendTo.Server)]
+    public void DebugLogStatsRpc()
+    {
+        Debug.Log($"Enemy Stats - " +
+            $"EnemyID: {CurrentStats.Value.EnemyID}, " +
+            $"Tier: {CurrentStats.Value.Tier}, " +
+            $"Health: {CurrentStats.Value.CurrentHealth}, " +
+            $"MoveSpeed: {CurrentStats.Value.MoveSpeed}, " +
+            $"ATKDamage: {CurrentStats.Value.ATKDamage}, " +
+            $"ATKSpeed: {CurrentStats.Value.ATKSpeed}" +
+            $"ATKRange: {CurrentStats.Value.ATKRange}");
+    }
+
+    [Rpc(SendTo.Server)]
+    public void RequestFireRpc()
+    {
+        if (Detector != null && Detector.NearestTarget != null)
+        {
+            NetworkObject targetNetObj = Detector.NearestTarget.GetComponent<NetworkObject>();
+            if (targetNetObj != null)
+            {
+                EnemyFireRpc(targetNetObj.NetworkObjectId);
+            }
+        }
+    }
+
+    [Rpc(SendTo.Everyone)]
+    private void EnemyFireRpc(ulong targetNetworkId)
+    {
+        if (BulletPrefab == null || ObjectPoolManager.Instance == null) return;
+
+        if (NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(targetNetworkId, out NetworkObject targetObj))
+        {
+            Vector3 spawnPos = transform.position;
+
+            Bullet bulletObj = ObjectPoolManager.Instance.SpawnObject<Bullet>(BulletPrefab, spawnPos, Quaternion.identity, PoolCategory.Projectiles);
+
+            if (bulletObj != null)
+            {
+                bulletObj.IsEnemy = true;
+                bulletObj.Initialize(targetObj.transform, CurrentStats.Value.ATKDamage);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, CurrentStats.Value.ATKRange);
+    }
+}
---
+++ Assets/Scripts/Manager/ObjectPoolManager.cs
@@ -0,0 +1,144 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Pool;
+using Unity.Netcode;
+
+public enum PoolCategory
+{
+    Default,
+    Projectiles,
+    UIVFX,
+    Enemies,
+    XP,
+    DamagePopup,
+    Audio
+}
+
+public class ObjectPoolManager : MonoBehaviour
+{
+    public static ObjectPoolManager Instance { get; private set; }
+
+    public int DefaultCapacity = 20;
+    public int MaxSize = 100;
+
+    private Dictionary<GameObject, ObjectPool<GameObject>> _prefabToPoolMap = new Dictionary<GameObject, ObjectPool<GameObject>>();
+    private Dictionary<GameObject, GameObject> _instanceToPrefabMap = new Dictionary<GameObject, GameObject>();
+    private Dictionary<PoolCategory, Transform> _categoryFolders = new Dictionary<PoolCategory, Transform>();
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+
+        InitFolder();
+    }
+
+    private void InitFolder()
+    {
+        foreach (PoolCategory category in System.Enum.GetValues(typeof(PoolCategory)))
+        {
+            GameObject folder = new GameObject($"{category}_Pool");
+            folder.transform.SetParent(transform);
+            _categoryFolders.Add(category, folder.transform);
+        }
+    }
+
+    private void CreatePool(GameObject prefab, PoolCategory category)
+    {
+        _prefabToPoolMap[prefab] = new ObjectPool<GameObject>(
+            createFunc: () =>
+            {
+                bool wasActive = prefab.activeSelf;
+                prefab.SetActive(false);
+
+                GameObject obj = Instantiate(prefab);
+
+                prefab.SetActive(wasActive);
+                return obj;
+            },
+            actionOnGet: (obj) =>
+            {
+            },
+            actionOnRelease: (obj) =>
+            {
+                obj.SetActive(false);
+
+                if (obj.TryGetComponent<NetworkObject>(out _))
+                {
+                    obj.transform.SetParent(null);
+                }
+                else
+                {
+                    obj.transform.SetParent(_categoryFolders[category]);
+                }
+            },
+            actionOnDestroy: (obj) => Destroy(obj),
+            collectionCheck: false,
+            defaultCapacity: DefaultCapacity,
+            maxSize: MaxSize
+        );
+    }
+
+    public T SpawnObject<T>(GameObject prefab, Vector3 position, Quaternion rotation, PoolCategory category = PoolCategory.Default, Transform parent = null) where T : UnityEngine.Object
+    {
+        if (prefab == null) return null;
+
+        if (!_prefabToPoolMap.ContainsKey(prefab))
+        {
+            CreatePool(prefab, category);
+        }
+
+        GameObject spawnedObj = _prefabToPoolMap[prefab].Get();
+
+        if (spawnedObj == null)
+        {
+            return SpawnObject<T>(prefab, position, rotation, category, parent);
+        }
+
+        if (parent != null)
+        {
+            spawnedObj.transform.SetParent(parent, false);
+        }
+        else
+        {
+            spawnedObj.transform.SetParent(null);
+        }
+
+        spawnedObj.transform.position = position;
+        spawnedObj.transform.rotation = rotation;
+
+        spawnedObj.SetActive(true);
+
+        _instanceToPrefabMap[spawnedObj] = prefab;
+
+        if (typeof(T) == typeof(GameObject))
+        {
+            return spawnedObj as T;
+        }
+
+        T component = spawnedObj.GetComponent<T>();
+        if (component == null)
+        {
+            Debug.LogError($"[ObjectPool] หา Component {typeof(T)} ไม่เจอใน {prefab.name}");
+            return null;
+        }
+
+        return component;
+    }
+
+    public void ReturnObjectToPool(GameObject instance)
+    {
+        if (instance == null || !_instanceToPrefabMap.ContainsKey(instance))
+        {
+            if (instance != null) Destroy(instance);
+            return;
+        }
+
+        GameObject originalPrefab = _instanceToPrefabMap[instance];
+
+        if (_prefabToPoolMap.ContainsKey(originalPrefab))
+        {
+            _prefabToPoolMap[originalPrefab].Release(instance);
+        }
+    }
+}
