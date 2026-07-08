e31b2818055c776fef90f5346a70a4e897ffa9b1
b_obbie <kobk708@gmail.com>
2026-07-08 17:45:36 +0700

    Add level 1 and increase debug XP gain
    Add level 1 as the starting progression level with 0 XP requirement. Increase debug XP gain from 100 to 5000 for faster testing during development.

--- Assets/Scripts/Modules/Player/Player.cs
+++ Assets/Scripts/Modules/Player/Player.cs
@@ -176,7 +176,7 @@ public class Player : NetworkBehaviour, IDamageble
         if (Keyboard.current.pKey.wasPressedThisFrame) Inventory.AddOrUpgradePassiveRpc("p2");
 
         if (Keyboard.current.lKey.wasPressedThisFrame) PlayerLevelManager.Instance.SharedLevel.Value += 1;
-        if (Keyboard.current.kKey.wasPressedThisFrame) PlayerLevelManager.Instance.RequestGainXPRpc(100);
+        if (Keyboard.current.kKey.wasPressedThisFrame) PlayerLevelManager.Instance.RequestGainXPRpc(5000);
 #endif
     }
 
